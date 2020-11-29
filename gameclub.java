87435f3929428a335b8772c34c808122b81078e8
xkobe8x <72715267+xkobe8x@users.noreply.github.com>
2020-11-29 16:49:23 -0500

    Add files via upload

---
+++ gameclub.java
@@ -0,0 +1,14 @@
+package com.example.organizationoutreach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gameclub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameclub);
+    }
+}
